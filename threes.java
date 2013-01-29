6b437421aa725a4e06c62bf69d8227e315e1eff9
Anirudh <mathukumillia@s.dcsdk12.org>
2013-01-29 08:11:11 -0700

    Added block comments

--- threes.java
+++ threes.java
@@ -1,3 +1,13 @@
+/**
+ *		threes
+ *			finds the number of threes in an array
+ *
+ * 		Anirudh Mathukumilli
+ *
+ *
+ * 
+ */
+
 public class threes {
 	public static void main(String[] args) {
 		int[] nums = {1,2,3,4,5,6,7,345,3,4,3,3};
@@ -5,6 +15,14 @@ public class threes {
 		System.out.println(getThrees(nums));
 	}
 
+	/**
+	 * 		getThrees
+	 * 		find the number of threes in an array
+	 *
+	 * 		@param nums - the array you want to find the number of threes in 
+	 * 		@return - the number of threes in the array
+	 * 
+	 */
 	public static int getThrees(int[] nums){
 		int counter = 0;
 		for(int i : nums){
