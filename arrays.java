4ce559b8a3a3ca95fe827de083abe3ffae4d6403
Anirudh <mathukumillia@s.dcsdk12.org>
2013-01-29 08:01:45 -0700

    Added in block comments

--- arrays.java
+++ arrays.java
@@ -1,4 +1,17 @@
+/**
+ *		arrays
+ *
+ * 			Methods for dealing with arrays
+ *
+ * 		Anirudh Mathukumilli
+ *
+ *
+ *
+ *
+ * 
+ */
 public class arrays {
+
 
 
 	public static void main(String[] args) {
@@ -7,6 +20,15 @@ public class arrays {
 		System.out.println(avg(nums));
 	}
 	
+
+	/**
+	 *  sum
+	 *  finds the sum of all the elements in an array
+	 *
+	 * 	@param nums - the array you want to find the sum of
+	 *
+	 * 	@return the sum of the elements in the array
+	 */
 	public static double sum(int[] nums){
 		double answer = 0;
 		for(int i = 0; i < nums.length; i++){
@@ -15,6 +37,16 @@ public class arrays {
 		return answer;
 	}
 
+
+	/**
+	 *	avg
+	 *  finds the mean of an array
+	 *
+	 * 	@param nums - the array you want to find the average of
+	 *
+	 * 	@return the mean of the array
+	 * 
+	 */
 	public static double avg(int[] nums){
 		double answer = sum(nums);
 
